050a13d2e9f321397cc989583f58cdc9977d4744
wilyanto <wilyanto24@gmail.com>
2021-01-12 03:16:31 +0700

    15

--- play.js
+++ play.js
@@ -1,16 +1,19 @@
-var name = 'Wilyanto';
-var age = 29;
-var hasHobbies = true;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return 'Name is ' + userName + ', age is ' + userAge + ', and the user has hobbyies: ' + userHasHobby;
+const person = {
+    name: 'Wilyanto',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
 }
 
-const add = (a, b) => a + b;
-const addOne = a => a + 1;
-const addRandom = () => 1 + 2;
+const hobbies = ['Sport', 8];
 
-console.log(summarizeUser(name, age, hasHobbies));
-console.log(add(1, 2));
-console.log(addOne(1));
-console.log(addRandom());
+// For each
+hobbies.forEach(hobby => {
+    console.log(hobby)
+});
+
+// Map array
+console.log(hobbies.map(hobby => {
+    return 'Hobby: ' + hobby;
+}));
