82a28a9e52f66de757336dc9e1f8e73bdf503d93
wilyanto <wilyanto24@gmail.com>
2021-01-12 12:21:21 +0700

    39

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http  = require('http');
+const fs = require('fs');
 
-// Giving response from request
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        console.log('hlo');
+        res.write('<html>');
+        res.write('<head><title>My Second Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit>Send/button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
