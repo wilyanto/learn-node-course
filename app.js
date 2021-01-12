447ff3ec71bc4407b313c632e8ed52401eaab66d
wilyanto <wilyanto24@gmail.com>
2021-01-12 11:49:41 +0700

    29

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http  = require('http');
 
+// Getting request attribute (headers, method, url, etc.)
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000);
