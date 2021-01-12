980e4c3159c55d3889f353828923524f2d5efa46
wilyanto <wilyanto24@gmail.com>
2021-01-13 02:56:18 +0700

    60

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http  = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use((req ,res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req ,res, next) => {
+    console.log('In another middleware');
+});
+
+const server = http.createServer(app);
+
+server.listen(8000);
