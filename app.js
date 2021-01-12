516446e3aba6c38eea8a11e39d3240a0965db06c
wilyanto <wilyanto24@gmail.com>
2021-01-12 11:55:00 +0700

    30

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http  = require('http');
 
-// Getting request attribute (headers, method, url, etc.)
+// Giving response from request
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
