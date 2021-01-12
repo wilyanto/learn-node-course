27bd1be6e3dcae169e3b3347be0b1744b70dc3b0
wilyanto <wilyanto24@gmail.com>
2021-01-12 12:56:43 +0700

    35

--- app.js
+++ app.js
@@ -17,15 +17,14 @@ const server = http.createServer((req, res) => {
         req.on('data', (chunck) => {
             console.log(chunck);
             body.push(chunck);
-        })
-        req.on('end', () => {
+        });
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+            fs.writeFileSync('message.txt', message);res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
