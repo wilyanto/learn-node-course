0440c1da14acb5366dc579bc39faca4f2f9ae00a
wilyanto <wilyanto24@gmail.com>
2021-01-13 13:28:45 +0700

    66

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const http  = require('http');
-
-const express = require('express');
+const express  = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req ,res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/product', (req ,res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req ,res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
