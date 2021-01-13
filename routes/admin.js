02986b6838da24087819d39c5f56d3c07f01a9b7
wilyanto <wilyanto24@gmail.com>
2021-01-13 14:07:00 +0700

    72

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const path = require('path');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req ,res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req ,res, next) => {
+router.post('/add-product', (req ,res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
