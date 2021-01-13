849f2e034da9e8d7ee300bb64df3f30186adb574
wilyanto <wilyanto24@gmail.com>
2021-01-14 00:21:06 +0700

    89

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
-const express = require('express');
-
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../utils/path');
 
@@ -8,13 +8,15 @@ const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req ,res, next) => {
-    res.render('add-product', {'pageTitle': 'Add Product'});
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 });
 
-router.post('/add-product', (req ,res, next) => {
-    products.push({'title': req.body.title});
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
