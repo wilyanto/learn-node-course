569e70e376ff31e9ec8e27b7166de60e9da6f576
wilyanto <wilyanto24@gmail.com>
2021-01-14 00:05:22 +0700

    85

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // send file as response, e.g: 404 Page HTML file
+    res.status(400).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
