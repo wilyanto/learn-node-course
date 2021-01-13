a533e8224a7594afee4971a7fe38265843eb99a7
wilyanto <wilyanto24@gmail.com>
2021-01-13 14:19:57 +0700

    75

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // send file as response, e.g: 404 Page HTML file
 });
 
 app.listen(3000);
