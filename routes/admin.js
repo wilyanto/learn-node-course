600319202fa628c6088f509e7af771c52cedea56
wilyanto <wilyanto24@gmail.com>
2021-01-13 13:56:06 +0700

    70

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/add-product', (req ,res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 router.post('/product', (req ,res, next) => {
