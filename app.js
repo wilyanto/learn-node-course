3d9a69479c6d703b2dca8bee759c06b3191030e2
wilyanto <wilyanto24@gmail.com>
2021-01-13 13:18:46 +0700

    64

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const http  = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req ,res, next) => {
-    console.log('In the middleware');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req ,res, next) => {
+    console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
 });
 
-app.use((req ,res, next) => {
-    console.log('In another middleware');
+app.use('/product', (req ,res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req ,res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
 
-server.listen(8000);
+server.listen(3000);
