6d3e4e0f94969cff57c4ec6b46ab88058a2c1f65
wilyanto <wilyanto24@gmail.com>
2021-01-14 21:57:21 +0700

    156

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,11 +15,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       console.log(product);
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
