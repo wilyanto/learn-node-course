d312cbd28fe44dd6481221ff6a2037a5f9e6b7ab
wilyanto <wilyanto24@gmail.com>
2021-01-14 20:03:03 +0700

    150

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    'database': 'node-course',
-    'password': '',
+const sequelize = new Sequelize("node-course", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
