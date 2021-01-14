caa1b8be2c3d4eb14f77c128b92c8617e28c2750
wilyanto <wilyanto24@gmail.com>
2021-01-14 17:45:59 +0700

    144

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const db = require('../util/database');
+const db = require("../util/database");
 
 const Cart = require("./cart");
 
@@ -11,7 +11,12 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
 
   static deleteById(id) {}
 
